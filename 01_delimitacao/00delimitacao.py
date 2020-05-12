34e342d97bc157f2fb90a05c57f12c7346c39020
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2020-05-12 13:33:08 -0300

    update delimitacao.py

--- 01_delimitacao/00delimitacao.py
+++ 01_delimitacao/00delimitacao.py
@@ -7,14 +7,16 @@ from osgeo import ogr
 
 ### Define variáveis do ambiente  ------------------------------
 
-VAZPATH = '/sysroot/home/eric/Documentos/Base de dados ZAP/zip 2406 reg vazao/'
+#VAZPATH = '/sysroot/home/eric/Documentos/Base de dados ZAP/zip 2406 reg vazao/'
+VAZPATH = 'C:/Users/gorge/Documents/GIS DataBase/izap input/'
 VAZFILE = '2406_JQ_Igam_UFV_reg_vazao_lin.shp'
 
-OTTOPATH = '/sysroot/home/eric/Documentos/Base de dados ZAP/zip 0102 ottobacias/'
+#OTTOPATH = '/sysroot/home/eric/Documentos/Base de dados ZAP/zip 0102 ottobacias/'
+OTTOPATH = 'C:/Users/gorge/Documents/GIS DataBase/izap input/'
 OTTOFILE = '0102_jq_otto_bacia_pol.shp'
 
-OUTPATH = '/sysroot/home/eric/Documentos/Base de dados ZAP/zip output/'
-
+#OUTPATH = '/sysroot/home/eric/Documentos/Base de dados ZAP/zip output/'
+OUTPATH = 'C:/Users/gorge/Documents/GitHub/i_zap/01_delimitacao/'
 CRS = QgsCoordinateReferenceSystem(4326, QgsCoordinateReferenceSystem.PostgisCrsId)
 CODRIO = "7582998"
 
@@ -31,7 +33,7 @@ hidrico.dataProvider().setEncoding(u'ISO-8859-1')
 ### Filtra o rio de interesse ---------------
 
 hidrico.setSubsetString("cocursodag LIKE '"+str(CODRIO)+"%'")
-QgsProject.instance().addMapLayer(hidrico)
+#QgsProject.instance().addMapLayer(hidrico)
 
 QgsVectorFileWriter.writeAsVectorFormat(hidrico, OUTPATH+'hidricoInteresse.shp', "utf-8", hidrico.crs(), "ESRI Shapefile")
 
@@ -41,9 +43,16 @@ otto = QgsVectorLayer(OTTOPATH+OTTOFILE, "Ottobacias", 'ogr')
 otto.setCrs(CRS)
 
 otto.setSubsetString("cocursodag LIKE '"+str(CODRIO)+"%'")
-QgsProject.instance().addMapLayer(otto)
+#QgsProject.instance().addMapLayer(otto)
 
 QgsVectorFileWriter.writeAsVectorFormat(otto, OUTPATH+'ottoInteresse.shp', "utf-8", otto.crs(), "ESRI Shapefile")
+
+### Criar limites da bacia
+processing.run("native:dissolve", {\
+'INPUT':OUTPATH+'ottoInteresse.shp',\
+'FIELD':[],\
+'OUTPUT':OUTPATH+'limiteBacia.shp'})
+
 
 ##QgsProject.instance().addMapLayer(dtmlayer)
 #extent = dtmlayer.extent()
