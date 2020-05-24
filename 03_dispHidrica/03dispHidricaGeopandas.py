4149cc83b77e47d44ebd765db54532f48e3708c8
gorgens <araguaia08>
2020-05-24 15:57:17 -0300

    Implementado disponibilidade hidrica

--- 03_dispHidrica/03dispHidricaGeopandas.py
+++ 03_dispHidrica/03dispHidricaGeopandas.py
@@ -11,9 +11,22 @@ limitCaptacao = 0.5
 # Join by location
 outorgasBacia = outorgasBacia.drop(['index_right'], axis=1)
 joinedOutorgas = gp.sjoin(outorgasBacia, ottoInteresse, op="within")
-print(joinedOutorgas.head())
+#print(joinedOutorgas.head())
+#print(joinedOutorgas[['Processo Outorga', 'cobacia', 'captacao']].head())
 
+grouped = joinedOutorgas.groupby('cobacia').sum()
+grouped.reset_index(inplace=True)
 
+grouped = grouped[['cobacia', 'captacao']]
+#print(grouped.head())
+
+hidricoInteresse = hidricoInteresse.merge(grouped, on='cobacia', how='outer')
+hidricoInteresse.rename(columns={'captacao': 'QdemTotal'}, inplace=True)
 
 # Calculos de disṕonibilidade
-# hidricoInteresse['QDH'] = limitCaptacao * hidricoInteresse['q7_10'] - hidricoInteresse['']
+hidricoInteresse['QDH'] = limitCaptacao * hidricoInteresse['q7_10'] - hidricoInteresse['QdemTotal']
+hidricoInteresse['compromDH'] = ((limitCaptacao * hidricoInteresse['q7_10']) - hidricoInteresse['QDH']) * 100 / (limitCaptacao * hidricoInteresse['q7_10'])
+hidricoInteresse['Qreg'] = (0.7 * hidricoInteresse['qmld_']) - (limitCaptacao * hidricoInteresse['q7_10'])
+hidricoInteresse['Viab'] = hidricoInteresse['Qreg'] + hidricoInteresse['QDH']
+
+hidricoInteresse.to_file("/home/gorgens/Github/i_zap/zapRibSantana.gpkg", layer='redeHidro', driver="GPKG")
